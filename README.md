cfd40c94e8d0a7bc2531f217d493e7e138c08cf2
Cathy Yu <cathyyu2018@gmail.com>
2022-12-10 17:12:14 -0800

    Add search by term to offical yelp api controller

--- README.md
+++ README.md
@@ -1,20 +1,2 @@
 # 5610-final-proj-node
 5610 final project node
-
-yelpAPIController usage:
-
-### Find businesses only support search by location.
-
-Sample url: `/api/businesses?location=seattle`
-
-Sample response: https://gist.github.com/cathyyucs/5f177831eb3fd0a4b9788df926a3914e
-
-Currently, returned number of businesses is hardcode at 4 for development purpose.
-
-`limit: '4'`
-
-### Find business detail info by business id
-
-Sample url: `/api/business/6I28wDuMBR5WLMqfKxaoeg`
-
-Sample response: https://gist.github.com/cathyyucs/69793a2d1675030bdb81e0912039556a
--- controller/yelpAPIController.js
+++ controller/yelpAPIController.js
@@ -3,13 +3,16 @@ import api from 'api';
 const sdk = api('@yelp-developers/v1.0#2hsur2ylbank95o');
 sdk.auth('Bearer Nha6KX2bDIn3g08t3sJ-HZ4rXpPJgQ_oLkiHTZ6nH5ELzcCi-xhDRX6R1qtom4jDoIsDuBCQHc_-ZBTU2xdgiNWI3PGLjIbSKDPNl7G3GrLVFqEPA7bUPpfzxm2RY3Yx');
 
+// Sample url: `/api/businesses?term=piazza&location=seattle`
 const findBusinesses = (req, res) => {
     const location = req.query.location;
-    sdk.v3_business_search({location: location, limit: '4'})
+    const term = req.query.term;
+    sdk.v3_business_search({term: term, location: location})
         .then(({data}) => {console.log(data);res.json(data);})
         .catch(err => console.error(err));
 }
 
+// Sample url: `/api/business/6I28wDuMBR5WLMqfKxaoeg`
 const findBusinessInfoById = (req, res) => {
     const business_id = req.params['bid'];
     sdk.v3_business_info({business_id_or_alias: business_id})

881ed441d63a0f0257a94892abb1857f3667b732
MengxiaoHuang <huang.meng@northeastern.edu>
2022-12-12 13:48:40 -0800

    revert push

--- models/country.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const countrySchema = new Schema({
-  name: String,
-  code: String
-});
-
-module.exports = mongoose.model('Country', countrySchema);
--- models/restaurant.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const restaurantSchema = new Schema({
-  name: String,
-  location: {type: {location: String, latitude: Number, longitude: Number}, required: true},
-});
-
-module.exports = mongoose.model('Restaurant', restaurantSchema);
